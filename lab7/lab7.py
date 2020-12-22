48035526e8a1d3c9224079787e5af437e2afd8d7
kwk18 <34220878+kwk18@users.noreply.github.com>
2020-12-23 00:58:38 +0300

    add comments =)

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -5,25 +5,25 @@ from matplotlib.widgets import Slider
 
 points = [[5, 0], [7, 1], [2, 2], [10, 3], [10, 4], [8, 5]]
 points = np.array(points)
-x = points[:, 0]
-t = points[:, 1]
+x = points[:, 0] # x = { 5, 7, 2, 10, 10, 8}
+t = points[:, 1] # t = { 0, 1, 2, 3, 4, 5}
 
 
-def interpol(x, t):
-    ipl_t = np.linspace(min(t), max(t), 100)
-    x_tup = si.splrep(t, x, k=4)
-    x_list = list(x_tup)
-    xl = x.tolist()
-    x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
-    x_i = si.splev(ipl_t, x_list)
-    return [ipl_t, x_i]
+def interpol(x, t):                          # interpolation of B-spline by x and t 
+    ipl_t = np.linspace(min(t), max(t), 100) # linear space 0..5, 100 elements
+    x_tup = si.splrep(t, x, k=4)             # find the B-spline representation
+    x_list = list(x_tup)                     # tuple to list 
+    xl = x.tolist()                          # np array to python list
+    x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]    
+    x_i = si.splev(ipl_t, x_list)            # evaluates a B-spline or its derivatives
+    return [ipl_t, x_i]                      # returns array of spline dots 
 
 
 fig = plt.figure()
 ax = fig.add_subplot(211)
-a, = plt.plot(t, x, '-og')
+a, = plt.plot(t, x, '-og')                   # plot container with knotes (by lines & dots)
 cords = interpol(x, t)
-l, = plt.plot(cords[0], cords[1], 'r')
+l, = plt.plot(cords[0], cords[1], 'r')       # plot array [x,y] for spline
 plt.xlim([min(t), max(t)])
 plt.ylim([0, 11])
 plt.title('B - Сплайн, n = 6, k = 4')
@@ -44,12 +44,12 @@ samp4 = Slider(axamp4, 'dot 4', 0.1, 10.0, valinit=points[4][0])
 samp5 = Slider(axamp5, 'dot 5', 0.1, 10.0, valinit=points[5][0])
 
 
-def update0(val):
-    amp = samp0.val
+def update0(val):  
+    amp = samp0.val 
     x[0] = amp
     cords = interpol(x, t)
-    l.set_ydata(cords[1])
-    a.set_ydata(x)
+    l.set_ydata(cords[1])  # upd data in l tuple
+    a.set_ydata(x)         # upd data in a tuple
 
 
 def update1(val):
